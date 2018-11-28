3c697c9b2611fe988d436504229dd3d625dc50a6
lealex262 <lealex262@gmail.com>
2018-11-27 20:45:11 -0500

    Added seam removal

--- code/seam_removing.py
+++ code/seam_removing.py
@@ -12,8 +12,24 @@ def determineClosestVLine(middle_pt, image):
 	return quad_line, quad_line[x_key], (middle_pt[1] < quad_line[1] and middle_pt[1] > quad_line[0])
 
 
-def deleteLines(im, direction, mask, bbox, side):
+def deleteLines(im, num, direction, mask, bbox, side):
+	# Energy Map
 	gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 	mag = filters.sobel(gray)
-	carved = transform.seam_carve(im, mag, 'vertical', 3)
+
+	# Maniplate Energy Regions
+	if side == 'up':
+		mag[:points[0][0], bbox[0][1]:bbox[1][1] + 1] = 1.0
+	elif side == 'bottom':
+		mag[points[1][0]:, bbox[0][1]:bbox[1][1] + 1] = 1.0
+	elif side == 'left':
+		mag[bbox[0][0]:bbox[1][0] + 1, :bbox[0][1]] = 1.0
+	else:
+		mag[bbox[0][0]:bbox[1][0] + 1, bbox[1][1]:] = 1.0
+
+	# Mask Energy Image
+	mag[mask] = 1.0
+
+	# New Carved Image
+	carved = transform.seam_carve(im, mag, direction, num)
 	return (carved * 255).astype('uint8')
